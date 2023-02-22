d8f96ab3008a5fd8710f692d408f04bc3e571540
Eric Krukowski <erickruk@amazon.com>
2023-02-21 21:42:08 -0500

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+function countDown(n){
+    while (n>=0) console.log(n--);
+}
+function writeCards(names, type){
+    const message = [];
+    for(let i =0; i<names.length;i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
+    }
+    return message;
+}
